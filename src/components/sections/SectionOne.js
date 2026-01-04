ee34ff91a86d0e93ee17e230c08669a3ef9ca00a
Admin <admin@Mac.Dlink>
2026-01-04 02:28:44 +0200

    section one continue

--- src/components/sections/SectionOne.js
+++ src/components/sections/SectionOne.js
@@ -1,24 +1,26 @@
 
+import Image from 'next/image'
 import React from 'react'
 
 
 function SectionOne() {
     const info = [
+
         {
-            icon: '🍽️',
+            image: '/croissant1.gif',
             title: 'אוכל טרי ומקומי',
             description: 'תפריט מגוון עם דגש על מרכיבים טריים ומקומיים המותאמים לעונות השנה.'
+        }, {
+            image: '/mood.gif',
+            title: 'אווירה נעימה',
+            description: 'מוזיקה חיה ואווירה חמימה שמזמינה אתכם להירגע וליהנות מהרגע.'
         },
         {
-            icon: '☕',
+            image: '/coffee1.gif',
             title: 'קפה איכותי',
             description: 'מבחר קפה מיוחד המגיע מזרעים מובחרים, קלוי במקום להבטחת טריות וטעם מעולה.'
         },
-        {
-            icon: '🎶',
-            title: 'אווירה נעימה',
-            description: 'מוזיקה חיה ואווירה חמימה שמזמינה אתכם להירגע וליהנות מהרגע.'
-        }
+
     ]
     return (
         <div className='bg-[url("/1.png")] bg-fixed bg-no-repeat bg-cover w-full h-full'>
@@ -29,9 +31,11 @@ function SectionOne() {
                     <div>
                         <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
                             {info.map((item, index) => (
-                                <div key={index} className='bg-text/80 rounded-lg p-6 shadow-lg text-center'>
-                                    <div className='text-5xl mb-4'>{item.icon}</div>
-                                    <h2 className='text-2xl font-semibold mb-2'>{item.title}</h2>
+                                <div key={index} className='bg-white rounded-lg p-6 shadow-lg text-center'>
+                                    <div className='text-5xl mb-4'>
+                                        <Image src={item.image} alt={item.title} className="mx-auto" width={232} height={232} />
+                                    </div>
+                                    <h2 className='text-2xl font-semibold mb-2 text-secondary'>{item.title}</h2>
                                     <p className='text-gray-700'>{item.description}</p>
                                 </div>
                             ))}
